9a231e18a4dd94460d4c9c980c4401ca92b017c4
Soren Umstot <soren.umstot@gmail.com>
2023-02-15 14:57:04 +0900

    Added phonenumber and ids

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,16 +3,19 @@ import Person from './components/Person'
 
 function App() {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {name: 'Arto Hellas', number: '040-123456', id: 1}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
     }
-    const checkPerson = persons.find(person => person.name.toLowerCase()=== personObject.name.toLowerCase())
+    const checkPerson = persons.find(person => person.name.toLowerCase() === personObject.name.toLowerCase())
     if (checkPerson) {
       alert(newName + 'is already listed')
       setNewName('')
@@ -20,17 +23,20 @@ function App() {
     else {
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-   const handleNameChange = (e) => {
-    console.log(e.target.value)
-    setNewName(e.target.value)
-   }
+  const handleNameChange = (e) => {
+  console.log(e.target.value)
+  setNewName(e.target.value)
+  }
 
-   const sameName =(persons, name)=> {
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
+  }
 
-   }
+
 
 
   return (
@@ -40,6 +46,7 @@ function App() {
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
