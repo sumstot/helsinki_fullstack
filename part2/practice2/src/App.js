911f21fefacb10301162a637f0eca01912c4ca93
Soren Umstot <soren.umstot@gmail.com>
2023-02-20 10:30:55 +0900

    practice 2 section c finished

--- part2/practice2/src/App.js
+++ part2/practice2/src/App.js
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note..'
   )
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
   const [showAll, setShowAll] = useState(true)
 
   const notesToShow = showAll
