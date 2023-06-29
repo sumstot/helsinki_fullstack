867788964fc2755fb455263a32a333bd502a273c
Soren Umstot <soren.umstot@gmail.com>
2023-06-29 22:32:21 +0900

    Generated new file for 3.1-3.8 project for Helsinki. Currently at 3.1

--- part3/practice/index.js
+++ part3/practice/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
+
 let notes = [
   {
     id: 1,
@@ -39,13 +45,31 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-app.delete('api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.filter(note => note.id !== id)
+  notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
-
 })
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  return maxId + 1
+}
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(404).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+  notes = notes.concat(note)
+  console.log(notes)
+  response.json(note)
+})
